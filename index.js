394ae4f47b1463dc8d57741bccccc189ad8f29d0
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-12-15 00:15:25 +0900

    chore: jestの設定を一度無効化

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 module.exports = {
   env: {
-    'browser': true,
-    'node': true,
-    'jest/globals': true,
+    browser: true,
+    node: true,
+    // 'jest/globals': true,
   },
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jest'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:jest/recommended', 'prettier'],
+  plugins: [
+    '@typescript-eslint',
+    // 'jest'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 'plugin:jest/recommended',
+    'prettier',
+  ],
   globals: {
     kintone: true,
     cybozu: true,
