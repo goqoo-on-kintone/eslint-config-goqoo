54097fed99cea4080b9ad27e1eeda0368db965e7
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-12-15 00:02:25 +0900

    feat: rulesなど一式作成

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 module.exports = {
+  env: {
+    'browser': true,
+    'node': true,
+    'jest/globals': true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:jest/recommended', 'prettier'],
+  globals: {
+    kintone: true,
+    cybozu: true,
+    jibun: true,
+    page: true,
+  },
+  parserOptions: { ecmaVersion: 2021 },
   rules: {
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-irregular-whitespace': ['error', { skipTemplates: true }],
+    'no-new': 'off',
     'no-var': 'error',
+    'prefer-const': 'error',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
   },
 }
